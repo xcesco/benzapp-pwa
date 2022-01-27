54632f8f39f1f97361f96b56cb35b0ad41196c5a
xcesco <xcesco@gmail.com>
2022-01-27 11:37:28 +0100

    @wip on layout

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,9 +4,11 @@ import { RouterModule } from '@angular/router';
 import { LOGIN_ROUTE } from './login.route';
 import { LoginComponent } from './login.component';
 import {SharedModule} from "../shared/shared.module";
+import {MatCardModule} from "@angular/material/card";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([LOGIN_ROUTE])],
+  imports: [SharedModule, RouterModule.forChild([LOGIN_ROUTE]), MatCardModule, FlexLayoutModule],
   declarations: [LoginComponent],
 })
 export class LoginModule {}
